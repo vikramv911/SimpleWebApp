9ab2524c79183f34885e56819b5318670247a2e8
vikramv911 <vikram.venkatesan@kellogg.ox.ac.uk>
2017-01-16 16:01:26 +0000

    trump test added

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -23,4 +23,8 @@ public class QueryProcessorTest {
     public void isNotCaseSensitive() throws Exception {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
+    @Test
+    public void knowsAboutTrump() throws Exception {
+        assertThat(queryProcessor.process("trump"), containsString("USA"));
+    }
 }
