d986b3192446033c213320ce2837e4952f499632
vikramv911 <vikram.venkatesan@kellogg.ox.ac.uk>
2017-01-16 14:35:36 +0000

    trump query added

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,6 +7,11 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else if (query.toLowerCase().contains("trump")) {
+            return "Donald Trump " +
+                    "will be the 45th president of the USA" +
+                    "on 20th January 2017.";
+        }
         return "";
     }
 }
